b76e43bf5f7e7e98d65161ba8d074650b9f6fe04
Léo Goemaere <goemaere.leo@hotmail.fr>
2018-10-09 20:23:26 +0200

    Adjust design + ceil input value

--- app/app.js
+++ app/app.js
@@ -1,26 +1,3 @@
-// const exercicesArray = [
-//     { 
-//         name: 'Squat',
-//         rm: null,
-//         tm: null
-//     },
-//     { 
-//         name: 'Bench',
-//         rm: null,
-//         tm: null 
-//     },
-//     { 
-//         name: 'deadlift',
-//         rm: null,
-//         tm: null 
-//     },
-//     { 
-//         name: 'Press',
-//         rm: null,
-//         tm: null 
-//     }
-// ];
-
 const exercices = {
     squat: { 
         name: 'Squat',
@@ -108,7 +85,7 @@ Vue.component('primary-exercice-item', {
     props: ['element', 'repetionmax', 'trainingmax'],
     template: `
         <div class="table__row">
-            <p class="table__cell--third">{{ element.name }}</p>
+            <p class="table__cell--quart">{{ element.name }}</p>
 
             <div class="table__cell--third input-container is-rm">
                 <input type="text" v-bind:value="element.rm" v-on:change="trainingmax($event, element)">
@@ -126,7 +103,7 @@ Vue.component('row-set-item', {
     template: `
         <div class="table__row table__row--set">
             <span class="item-number">{{ index + 1 }}</span>
-            <p><span class="repet-number">{{ element.reps }}</span> @ {{ (exercice.tm) * (element.tm) }}</p>
+            <p><span class="repet-number">{{ element.reps }}</span> @ <span class="weight-item">{{ Math.ceil( ((exercice.tm) * (element.tm)) * 4) / 4 }}</span> kg</p>
         </div>
     `
 })
@@ -142,18 +119,18 @@ const app = new Vue({
         calculateTrainingMax: function(e, item) {
             const repetionMax = e.target.value;
             const trainingMax = repetionMax * 0.9;
-            item.tm = trainingMax;
-            item.rm = repetionMax;
+            item.tm = Math.ceil(trainingMax * 4) / 4;
+            item.rm = Math.ceil(repetionMax * 4) / 4;
         },
         calculateRepetitionMax: function(e, item) {
             const trainingMax = e.target.value;
             const repetitionMax = trainingMax / 0.9;
-            item.rm = repetitionMax;
-            item.tm = trainingMax;
+            item.rm = Math.ceil(repetitionMax * 4) / 4;
+            item.tm = Math.ceil(trainingMax * 4) / 4;
         },
         toggleClass: function(e) {
-            e.target.parentNode.nextElementSibling.classList.toggle('table__content--hide');
-            // console.log(e.target.parentNode.nextElementSibling);
+            e.target.classList.toggle('active');
+            e.target.nextElementSibling.classList.toggle('table__content--hide');
         }
     }
 });
